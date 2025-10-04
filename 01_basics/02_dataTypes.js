9f525b7fad4ee7005541f67e36ed5e6bc9db1cfa
Ashish Srivastava <ashi.sri.18@gmail.com>
2025-10-04 18:48:14 +0000

    primitive and non-primitive datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,13 +2,11 @@
 
 // alert(3+3) // we are using nodejs, not browser
 
-let name = "Ashish"
-let age = 33
-let isLoggedIn = false
-let state;
+// Types of DataTypes: Premitive and Non-Premitive
 
+console.log("=======Primitive==========");
+// 1. Premitive: Call By Value
 
-// DataTypes:
 // number => 2 to power 53
 // bigint
 // string => ""
@@ -17,15 +15,42 @@ let state;
 // undefined =>
 // symbol => uniuqe
 
-// object
+let fName = "Ashish"
+let age = 33
+let isLoggedIn = false
+let state;
+const marks = 75.5
+const temp = null
+const id = Symbol('123')
+const anotherId = Symbol('123')
 
-console.log(typeof "Ashish");       // string
-console.log(typeof name);           // string
+console.log(typeof "Ashish Sri");   // string
+console.log(typeof fName);          // string
 console.log(typeof age);            // number
 console.log(typeof isLoggedIn);     // boolean
+console.log(typeof state);          // undefined
 console.log(typeof undefined);      // undefined
 console.log(typeof null);           // object
+console.log(typeof marks);          // number
+console.log(typeof temp);           // object
+console.log(typeof id);             // symbol
+console.log(id === anotherId);      // false -> because Symbols are always unique
 
 
+console.log("=======Non-Primitive==========");
+// 2. Non-Premitive: Call By Reference 
 
+// Array, Objects, Functions
 
+const heros = ["Shaktiman", "Naagraj", "Doga"]
+let myObj = {
+    myName: "Ashish Srivastava",
+    myAge: "36"
+}
+const myFunction = function(){
+    console.log("Hello World");    
+}
+
+console.log(typeof heros);      // object
+console.log(typeof myObj);      // object
+console.log(typeof myFunction); // object
